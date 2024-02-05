5fb006f1fd4572c53c17b4aec6e9c5271892ba5a
htetzawphyo <htetzawphyo.hpn@gmail.com>
2024-02-05 10:49:22 +0630

    upgrade file upload size validation

--- app/Http/Requests/File/FileUpdateRequest.php
+++ app/Http/Requests/File/FileUpdateRequest.php
@@ -23,7 +23,7 @@ class FileUpdateRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'file' => 'required|mimes:pdf|max:5000'
+            'file' => 'required|mimes:pdf|max:10240'
         ];
     }
 
--- app/Http/Requests/File/FileUploadRequest.php
+++ app/Http/Requests/File/FileUploadRequest.php
@@ -23,7 +23,7 @@ class FileUploadRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'file' => 'required|mimes:pdf|max:5000'
+            'file' => 'required|mimes:pdf|max:10240'
         ];
     }
 
